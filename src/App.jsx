a268640f965a3be8de7dd9e16a92f8b15a285d35
KSudhaMadhuri <ksmdhuri736@gmail.com>
2024-10-11 02:33:28 +0530

    sudhasNav

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { useState } from 'react'
 
 import './App.css'
-import Navbar from './assets/Navbar'
+// import Navbar from './assets/Navbar'
 import { Routes,Route } from 'react-router-dom'
 import Cart from './assets/Cart'
 import Footer from './assets/Footer'
 import Home from './assets/Home'
 import ContactUs from './assets/ContactUs'
 import Search from './assets/Search'
+import IskconNav from './assets/IskconNav'
 
 
 function App() {
 
   return (
     <>
-<Navbar/>
+{/* <Navbar/> */}
+<IskconNav/>
 <Routes>
-<Route path="/" element={<Home />} />
+<Route path="/" element={<Home/>} />
 <Route path="/cart" element={<Cart/>} />
 <Route path="/footer" element={<Footer/>} />
 <Route path="/contact" element={<ContactUs/>} />
