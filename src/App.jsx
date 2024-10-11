384e0e93886cfd9fe401d3a19881e7ab64e7ba02
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-10-11 08:45:00 +0530

    added home page api and product overview api and ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,50 @@
-import { useState } from 'react'
-
+import { createContext, useState, useEffect } from 'react'
 import './App.css'
-// import Navbar from './assets/Navbar'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Cart from './assets/Cart'
 import Footer from './assets/Footer'
 import Home from './assets/Home'
 import ContactUs from './assets/ContactUs'
 import Search from './assets/Search'
-import IskconNav from './assets/IskconNav'
+import Navbar from './assets/Navbar'
+import axios from 'axios'
+import ProductOverView from './assets/ProductOverView'
 
+export const productsContext = createContext()
 
 function App() {
+  const api = import.meta.env.VITE_API;
+  const [products, setProducts] = useState([])
+ 
+
+  // fetching books data from server
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await axios.get(`${api}/book/getbooks`);
+        if (response) {
+          setProducts(response.data.reverse());
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchBooks();
+  }, []);
 
   return (
     <>
-{/* <Navbar/> */}
-<IskconNav/>
-<Routes>
-<Route path="/" element={<Home/>} />
-<Route path="/cart" element={<Cart/>} />
-<Route path="/footer" element={<Footer/>} />
-<Route path="/contact" element={<ContactUs/>} />
-<Route path="/search" element={<Search/>} />
-
-
-
-</Routes>
+      <productsContext.Provider value={{ products, setProducts }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/footer" element={<Footer />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/search" element={<Search />} />
+          <Route path='/:bookId' element={<ProductOverView/>}/>
+        </Routes>
+      </productsContext.Provider>
     </>
   )
 }
--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -1,8 +1,193 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { productsContext } from '../App'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+  const { products } = useContext(productsContext)
+
   return (
-    <></>
+    <>
+      <div className="bg-white pt-0" >
+        <div className="mx-auto max-w-2xl px-4 py-10 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+
+          <div className='flex justify-between'>
+            <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+              Latest Products
+            </h2>
+
+
+
+          </div>
+          <hr className='my-3' />
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+
+            {products.length ? <>
+              {products.map((item) => (
+
+                <Link to={`/${item._id}`} key={item.id} className="group w-72 p-3 relative  hover:opacity-85">
+                  <div className=''>
+                    <img
+                      src={item.bookImage}
+                      alt={item.bookName}
+                      className="h-72 w-72 rounded"
+                    />
+                  </div>
+
+                  <div className="mt-4 flex justify-between ">
+                    <div>
+                      <h3 className="text-sm text-gray-700">
+                        <a href="#">
+                          <span aria-hidden="true" className="absolute inset-0" />
+                          {item.bookName.substring(0, 22)}..
+                        </a>
+                      </h3>
+                    </div>
+                    <p className="text-sm font-medium text-nowrap text-gray-900">₹ {item.bookPrice}</p>
+                  </div>
+                </Link>
+              ))}
+            </> : <>
+
+              <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div> <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div> <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div> <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div> <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div> <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div> <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div> <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div> <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div> <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div> <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div> <div className="w-72 animate-pulse rounded">
+                <div className="bg-slate-400 rounded h-72 w-72 "
+                ></div>
+                <div className="mt-4 flex justify-between ">
+                  <div>
+                    <h3 className="text-sm  bg-slate-400 h-4 w-72 ">
+                    </h3>
+                    <h3 className="text-sm mt-2 bg-slate-400 h-4 w-48 ">
+                    </h3>
+                  </div>
+                </div>
+              </div>
+
+            </>
+            }
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
